c156eec2bebfd26258ba2f9ea81f8df11b7b722c
safiul <safiul.alam@nordcloud.com>
2024-04-24 21:56:42 +0300

    Update bank details display in SellerView component

--- src/content/pages/PDFProformaInvoice/SellerView.tsx
+++ src/content/pages/PDFProformaInvoice/SellerView.tsx
@@ -40,37 +40,39 @@ const SellerView = ({ styles, View, Text, sample_data }) => {
             ) : null}
           </View>
         </View>
-        <View style={styles.comments}>
-          <View>
-            <Text style={styles.manufacturertitle}>
-              Manufacturer's Bank Details
-            </Text>
-            <Text style={{ marginBottom: 2 }}>
-              <Text style={{ color: '#6b6b6b' }}>
-                Account Name: {sample_data?.bank_details?.accountname}
+        {sample_data?.bank_details ? (
+          <View style={styles.comments}>
+            <View>
+              <Text style={styles.manufacturertitle}>
+                Manufacturer's Bank Details
               </Text>
-            </Text>
-            <Text style={{ marginBottom: 2 }}>
-              <Text style={{ color: '#6b6b6b' }}>
-                IBAN: {sample_data?.bank_details?.iban}
-              </Text>
-            </Text>
-            <Text style={{ marginBottom: 2, color: '#6b6b6b' }}>
-              BIC:{' '}
-              <Text style={{ color: '#6b6b6b' }}>
-                {sample_data?.bank_details?.bic}
-              </Text>
-            </Text>
-            {sample_data?.bank_details?.iban ? (
-              <Text style={{ marginBottom: 2, color: '#6b6b6b' }}>
-                Bank:{' '}
+              <Text style={{ marginBottom: 2 }}>
                 <Text style={{ color: '#6b6b6b' }}>
-                  {sample_data?.bank_details?.bank}
+                  Account Name: {sample_data?.bank_details?.accountname}
                 </Text>
               </Text>
-            ) : null}
+              <Text style={{ marginBottom: 2 }}>
+                <Text style={{ color: '#6b6b6b' }}>
+                  IBAN: {sample_data?.bank_details?.iban}
+                </Text>
+              </Text>
+              <Text style={{ marginBottom: 2, color: '#6b6b6b' }}>
+                BIC:{' '}
+                <Text style={{ color: '#6b6b6b' }}>
+                  {sample_data?.bank_details?.bic}
+                </Text>
+              </Text>
+              {sample_data?.bank_details?.iban ? (
+                <Text style={{ marginBottom: 2, color: '#6b6b6b' }}>
+                  Bank:{' '}
+                  <Text style={{ color: '#6b6b6b' }}>
+                    {sample_data?.bank_details?.bank}
+                  </Text>
+                </Text>
+              ) : null}
+            </View>
           </View>
-        </View>
+        ) : null}
       </View>
     </>
   );
